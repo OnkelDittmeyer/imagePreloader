dcc9a2065666fa2212f9688b07806bac4cf4707e
OnkelDittmeyer <helloworld@marcelhelmer.de>
2015-12-14 21:52:02 +0000

    Update listImages.rb

--- listImages.rb
+++ listImages.rb
@@ -14,12 +14,7 @@ class ListImages
           n += 1
         end
 
-       #File.open("temp.json","w") do |f|
-         #f.write(tempHash.to_json)
-       #end
-
       [200,{"Content-Type" => "application/javascript"}, [tempHash.to_json]]
-      # the json content i want to return, couldnt find a good referene about the correct structure here
    puts "All done!"
    end
 end
